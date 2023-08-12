9aec5b6ee0bc855bba2ab27ec3ca5d51650912ed
Gaurav <gauravbisht154@gmail.com>
2023-08-12 21:47:47 +0530

    repo initialized

---
+++ Server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+// middleware
+app.use(express.static("public"));
+
+// view engine
+app.set("view engine", "ejs");
+
+// database connection
+const dbURI =
+  "mongodb+srv://JWT:EHBlE1wec22KEwOb@cluster0.ei5qidr.mongodb.net/JWTauth";
+mongoose
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useCreateIndex: true,
+  })
+  .then((result) => app.listen(3000, () => console.log("server is running")))
+  .catch((err) => console.log(err));
+
+// routes
+app.get("/", (req, res) => res.render("home"));
+app.get("/smoothies", (req, res) => res.render("smoothies"));
