cda5bdd95acb31b516b24f233d9a866bce0f4580
Gaurav <gauravbisht154@gmail.com>
2023-08-21 13:32:19 +0530

    controller and routes  updated

---
+++ routes/authRoutes.js
@@ -0,0 +1,11 @@
+const { Router } = require("express");
+const router = Router();
+const authController = require("../controllers/authController");
+
+router.get("/signup", authController.signup_get);
+router.get("/login", authController.login_get);
+router.post("/signup", authController.signup_post);
+router.post("/login", authController.login_post);
+router.get("/logout", authController.logout_get);
+
+module.exports = router;
