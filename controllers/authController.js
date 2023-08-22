661bc9ac75480d297d8293acdcbf153d69f75bff
Gaurav <gauravbisht154@gmail.com>
2023-08-22 11:30:48 +0530

    validation check added for email and password to ensure correct data imbuing into db

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,25 @@
 const User = require("../models/User");
 
+// handling errors comming from db due to validation issues
+const handleErrors = (err) => {
+  let errors = { email: "", password: "" };
+
+  //validation errors
+  if (err.message.inclides("user validation failed")) {
+    Object.values(err.errors).forEach(({ properties }) => {
+      errors[properties.path] = properties.message;
+    });
+  }
+
+  //duplicate error code
+  if (err.code === 11000) {
+    errors.email = "Email already registered";
+    return errors;
+  }
+
+  return errors;
+};
+
 module.exports.signup_get = (req, res) => {
   res.render("signup");
 };
@@ -15,8 +35,8 @@ module.exports.signup_post = async (req, res) => {
     const user = await User.create({ email, password });
     res.status(201).json(user);
   } catch (err) {
-    console.log(err);
-    res.status(400).send("error, user not created");
+    const errors = handleErrors(err);
+    res.status(400).json(errors);
   }
 };
 
--- model/User.js
+++ model/User.js
@@ -1,16 +1,19 @@
 const mongoose = require("mongoose");
+const { isEmail } = require("validator");
+
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
-    required: true,
+    required: [true, "Email is required"],
     unique: true,
     lowercase: true,
+    validation: [isEmail, "Please enter a valid email"],
   },
   password: {
     type: String,
-    required: true,
-    minlength: 6,
-    maxlength: 16,
+    required: [true, "Password is required"],
+    minlength: [6, "Minimum password length is 6 characters"],
+    maxlength: [16, "Maximum password length is 16 characters"],
   },
 });
 
